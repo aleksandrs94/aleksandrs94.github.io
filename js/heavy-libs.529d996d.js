c09ac3215423575ab02107b4c6fd94db870cb241
Aleksandrs Sprudzans <aleksandrs.sprudzans@gmail.com>
2026-02-23 13:48:01 +0800

    Improve mobile navigation overflow

--- js/heavy-libs.529d996d.js
+++ js/heavy-libs.529d996d.js
@@ -1,4 +1,4 @@
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"23586eb153f35ceb6901bdfbd9aa8200292ac2d1"};var t=(new e.Error).stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="1556189e-476b-4cd1-9b10-a33f276169a4",e._sentryDebugIdIdentifier="sentry-dbid-1556189e-476b-4cd1-9b10-a33f276169a4")}catch(e){}}(),(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["heavy-libs"],{e0ec:function(e,t,n){(function(e){
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"00e1cb1869ec0d41ba771af2f6d99c409f3de768"};var t=(new e.Error).stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="1556189e-476b-4cd1-9b10-a33f276169a4",e._sentryDebugIdIdentifier="sentry-dbid-1556189e-476b-4cd1-9b10-a33f276169a4")}catch(e){}}(),(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["heavy-libs"],{e0ec:function(e,t,n){(function(e){
 /*!
  * vue-youtube v1.4.0
  * (c) 2019 Antério Vieira
